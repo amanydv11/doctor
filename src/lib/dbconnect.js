bfde6750cffae34f2c6fa555937014fc408fe5b7
aman <amanraj1172004@gmail.com>
2025-05-02 14:28:30 +0530

    project setup

---
+++ src/lib/dbconnect.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+async function connectDB() {
+  if (connection.isConnected) {
+    console.log('database conection is already exist');
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || '', {
+      bufferCommands: false,
+    });
+    
+    connection.isConnected = db.connections[0].readyState;
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1);
+  }
+}
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from MongoDB');
+  connection.isConnected = false;
+});
+
+
+process.on('SIGINT', async () => {
+  try {
+    await mongoose.connection.close();
+    console.log('Mongoose connection closed through app termination');
+    process.exit(0);
+  } catch (err) {
+    console.error('Error during app termination:', err);
+    process.exit(1);
+  }
+});
+
+export default connectDB;
